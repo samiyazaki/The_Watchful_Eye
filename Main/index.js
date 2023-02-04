895f6b0394640394d1ded9df23d3b551fe2fc025
Seiya Miyazaki <smsmiyazaki@gmail.com>
2023-02-04 08:13:38 -0500

    initial set up

---
+++ Main/index.js
@@ -0,0 +1,35 @@
+const cTable = require('console.table');
+const mysql = require('mysql2');
+var inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        // Leave empty for now, this is where the quesions go
+    ])
+    .then((answers) => {
+        //Use what they say to do stuff
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered
+        } else {
+            //Something went wrong
+        }
+    });
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Rootsql',
+    database: 'test'
+});
+
+console.table([
+    {
+        name: 'foo',
+        age: 10
+    }, {
+        name: 'bar',
+        age: 20
+    }
+]);
